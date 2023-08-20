fa7b29b1eda68eb543eb1a14e5fd23f8dd14b246
Hareem Gohar <malikzarsh2@gmail.com>
2023-08-20 14:04:54 +0500

    first commit

---
+++ src/Component/Parent.jsx
@@ -0,0 +1,36 @@
+// ParentComponent.js
+import React, { useState } from 'react';
+import ItemList from './child';
+
+function ParentComponent() {
+  const [items, setItems] = useState([]);
+
+  //  add a new item 
+  const addItem = (newItem) => {
+    setItems([...items, newItem]);
+  };
+
+  // update an existing item
+  const updateItem = (index, updatedItem) => {
+    const updatedItems = [...items];
+    updatedItems[index] = updatedItem;
+    setItems(updatedItems);
+  };
+
+  //  delete an item
+  const deleteItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div>
+      <h1 className='text-center p-2' >CRUD-Functions</h1>
+      <hr />
+      <ItemList items={items} onAddItem={addItem} onUpdateItem={updateItem} onDeleteItem={deleteItem} />
+    </div>
+  );
+}
+
+export default ParentComponent;
